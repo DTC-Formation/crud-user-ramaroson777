315f03c4232c18e77d431e1eee0560021beb8759
ramaroson777 <ramaroson777@gmail.com>
2023-07-04 01:00:12 +0300

    personnalisation du formulaire

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,11 +14,38 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom')
-            ->add('prenom')
-            ->add('age')
-            ->add('cin')
-            ->add('adresse')
+            ->add('nom',TextType::class,[
+                'label' => 'Entrer votre nom :',
+                "attr" => [
+                    "class" => "nom-input",
+                    "placeholder" => "Votre nom" 
+                ]
+            ])
+            ->add('prenom',TextType::class,[
+                'label' => 'Entrer votre prenom :',
+                "attr" => [
+                    "class" => "prenom-input",
+                    "placeholder" => "Votre prenom" 
+                ]
+            ])
+            ->add('age', IntegerType::class,[
+                "attr" => [
+                    "class" => "age-input",
+                    "placeholder" => "Votre age" 
+                ]
+            ])
+            ->add('cin', TextType::class,[
+                "attr" => [
+                    "class" => "cin-input",
+                    "placeholder" => "Votre CIN" 
+                ]
+            ])
+            ->add('adresse',TextType::class,[
+                "attr" => [
+                    "class" => "adresse-input",
+                    "placeholder" => "Votre adresse" 
+                ]
+            ])
         ;
     }
 
